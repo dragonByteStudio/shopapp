0eaf4ede3d261d03508a49e9e789b57fd7c111af
aprather <aprather@gmail.com>
2019-09-24 14:32:48 -0500

    package.json modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+/* Components */
+//import Intro from './components/Intro';
+//import Login from './components/Login';
+//import LoginSuccess from './components/LoginSuccess';
+//import SignUp from './components/SignUp';
+//import SignUpSuccess from './components/SignUpSuccess';
+//import Loader from './components/Loader';
+//import Captcha from './components/Captcha';
+import Success from './components/Success';
+
+/* Style */
+// import './index.css';
+import './assets/style/global.css';
+
 import * as serviceWorker from './serviceWorker';
+
+function App() {
+	return (
+		<div className={'mobile'}>
+			<Success />
+		</div>
+	);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
